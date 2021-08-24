c0308df833b3f8a602e56adf15b17509de80d860
Antonio Cunanan <antonio.cunanan@gmail.com>
2021-08-24 09:30:15 -0500

    Complete approve function

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -152,23 +152,30 @@ contract ERC721 is Pausable, ERC165 {
     function balanceOf(address owner) public view returns (uint256) {
         // TODO return the token balance of given address
         // TIP: remember the functions to use for Counters. you can refresh yourself with the link above
+        return _ownedTokensCount[owner].current();
     }
 
     function ownerOf(uint256 tokenId) public view returns (address) {
         // TODO return the owner of the given tokenId
+        return _tokenOwner[tokenId];
     }
 
 //    @dev Approves another address to transfer the given token ID
     function approve(address to, uint256 tokenId) public {
-        
-        // TODO require the given address to not be the owner of the tokenId
 
-        // TODO require the msg sender to be the owner of the contract or isApprovedForAll() to be true
+        address tokenOwner = ownerOf(tokenId);
+
+        // TODO require the given address to not be the owner of the tokenId
+        require(to != tokenOwner, "You are the owner of the token");
+
+        // TODO require the msg sender to be the owner of the token or isApprovedForAll() to be true
+        require(msg.sender == tokenOwner || isApprovedForAll(tokenOwner, msg.sender) == true);
 
         // TODO add 'to' address to token approvals
+        _tokenApprovals[tokenId] = to;
 
         // TODO emit Approval Event
-
+        emit Approval(tokenOwner, to, tokenId);
     }
 
     function getApproved(uint256 tokenId) public view returns (address) {
