1a65535745de50530679173fba44f5525d3261a7
Antonio Cunanan <antonio.cunanan@gmail.com>
2021-08-28 00:14:33 -0500

    Update solution square verifier

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,34 +1,46 @@
 pragma solidity >=0.4.21 <0.6.0;
 
+import "./ERC721Mintable.sol";
+import "./verifier.sol";
+
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
-
-
+contract SquareVerifier is Verifier {}
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is CustomERC721Token {
+
+  SquareVerifier squareVerifier;
+
+  constructor(address verifierContract) public {
+    squareVerifier = SquareVerifier(verifierContract);
+  }
+
+  // TODO define a solutions struct that can hold an index & an address
+
+
+  // TODO define an array of the above struct
+
+
+  // TODO define a mapping to store unique solutions submitted
 
 
 
-// TODO define a solutions struct that can hold an index & an address
-
-
-// TODO define an array of the above struct
-
-
-// TODO define a mapping to store unique solutions submitted
+  // TODO Create an event to emit when a solution is added
 
 
 
-// TODO Create an event to emit when a solution is added
+  // TODO Create a function to add the solutions to the array and emit the event
 
 
 
-// TODO Create a function to add the solutions to the array and emit the event
+  // TODO Create a function to mint new NFT only after the solution has been verified
+  //  - make sure the solution is unique (has not been used before)
+  //  - make sure you handle metadata as well as tokenSuplly
+
+}
 
 
 
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
 
   
 
--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,13 @@
 // migrating the appropriate contracts
-var SquareVerifier = artifacts.require("./verifier.sol");
-// var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+const SquareVerifier = artifacts.require("./verifier.sol");
+const SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SquareVerifier);
-  // deployer.deploy(SolnSquareVerifier);
+module.exports = function (deployer) {
+  deployer
+    .deploy(SquareVerifier)
+    .then(() => {
+			return deployer
+				.deploy(SolnSquareVerifier, SquareVerifier.address);
+    });
 };
+
